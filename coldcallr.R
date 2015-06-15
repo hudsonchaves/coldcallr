ec3cc79270122cde22fe47d3e88951943c62395c
mostly-harmless <vnijs@ucsd.edu>
2015-06-14 22:33:59 -0700

    group_name as.factor

--- coldcallr.R
+++ coldcallr.R
@@ -20,6 +20,8 @@ student_info <- function(section, spath, img_type = ".jpg") {
     arrange(last_name, first_name)
     sinfo
 
+  sinfo$group_name %<>% as.factor
+
   sinfo %<>% mutate(pref_name = ifelse(pref_name == "", first_name, pref_name))
   sinfo$id <- paste0(sinfo$first_name, ".", sinfo$last_name)
   sinfo$images <- paste0(spath, "/", section, "/", sinfo$id, img_type)
