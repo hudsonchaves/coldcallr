42f1b09959551658ac5c3509f9e0cffe5c01ba48
mostly-harmless <vnijs@ucsd.edu>
2015-06-14 22:14:06 -0700

    Fixed image issue using addResourcePath

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # coldcallr
-Cold call your students. They will love it! 
 
-Uses rmarkdown and shiny
+Cold call your students. They will love it!
+
+Open coldcallr.Rmd in Rstudio and press `Run Document`. Output can be shown/hidden by clicking the black triangles.
+
+## Cold call
+
+Press the cold call button to select one more students to call on.
+
+## Student list
+
+A full list of students that can be sorted, searched, etc.
+
+## Names
+
+Student pictures in random order. Press the `Shuffle` button to reorder. Hover over the image to see the student's name.
+
+## Requirements
+
+Uses rmarkdown, shiny, and DT. As DT is not on CRAN yet install the package from rstudio/DT on github using devtools or using the command below:
+
+`install.packages("DT", repos = "http://vnijs.github.io/radiant_miniCRAN/")`
+
+Also requires a csv file with students names and student picture. See the content of the student-info directory for details.
--- coldcallr.R
+++ coldcallr.R
@@ -10,25 +10,28 @@ library(DT)
 knitr::opts_chunk$set(echo = FALSE, comment = NA, cache = FALSE,
                       message = FALSE, warning = FALSE)
 
+## if you are only using renderTable / xtable without col or row names
+# options(xtable.include.rownames=F)
+# options(xtable.include.colnames=F)
+
 student_info <- function(section, spath, img_type = ".jpg") {
 
-  sinfo <- read.csv(paste0(spath, section, ".csv"), as.is = TRUE) %>%
+  sinfo <- read.csv(paste0(spath, "/", section, ".csv"), as.is = TRUE) %>%
     arrange(last_name, first_name)
     sinfo
 
   sinfo %<>% mutate(pref_name = ifelse(pref_name == "", first_name, pref_name))
   sinfo$id <- paste0(sinfo$first_name, ".", sinfo$last_name)
-  sinfo$images <- paste0(spath, section, "/", sinfo$id, img_type)
+  sinfo$images <- paste0(spath, "/", section, "/", sinfo$id, img_type)
 
   ## if an image is missing use a standard image
   sinfo$images[!file.exists(sinfo$images)] <- "student-info/zzz111.jpg"
 
-  ## if images hosted on remove server
+  ## if images are hosted on a remove server
   # site_path <- "http://your-remote-server-name/site_media/"
   # images <- paste0(site_path,images)
 
   sinfo$images <- with(sinfo, paste0("<img src='", images,"' title='", pref_name, " ", last_name,"' style='height:60px'>"))
-
   sinfo
 }
 
@@ -39,10 +42,18 @@ mem_names <- function(sinfo, nr_col = 8) {
   sinfo$images %<>% sub("height:[0-9]{2,3}px", "height:120px", .)
   nr <- nrow(sinfo)
   ssample <- sample(sinfo$images, nr)
-  rndmat <- matrix("", ncol = nr_col, nrow = ceiling(nr / nr_col)) %>%
+  rndmat <- matrix(NA, ncol = nr_col, nrow = ceiling(nr / nr_col)) %>%
     set_colnames(rep("", nr_col))
   rndmat[1:nr] <- ssample
-  kable(rndmat, format = "markdown")
+  rndmat[ ,apply(rndmat, 2, function(x) !any(is.na(x)))]
+}
+
+mem_table <- function(sinfo, btn, nr_col = 8) {
+  renderTable({
+    if(input[[btn]] == 0) return()
+    mem_names(sinfo, nr_col)
+  }, include.colnames = FALSE, include.rownames = FALSE,
+     sanitize.text.function = identity)
 }
 
 cold_call <- function(sinfo, nr = 5) {
@@ -55,7 +66,44 @@ cold_call <- function(sinfo, nr = 5) {
     mutate(rnum = round(rnum,3))
 }
 
+DT_simple <- function(sinfo, btn, nr = 4) {
+  DT::renderDataTable({
+    if(input[[btn]] == 0) return()
+    cold_call(sinfo, nr = nr) %>%
+      DT::datatable(rownames = FALSE, style = "bootstrap", escape = FALSE,
+        options = list(
+          paging = FALSE,
+          searching = FALSE,
+          searchable = FALSE,
+          columnDefs = list(list(className = 'dt-center', targets = "_all")),
+          autoWidth = TRUE,
+          processing = FALSE
+        )
+      )
+  })
+}
+
+DT_full <- function(sinfo) {
+  DT::renderDataTable({
+    DT::datatable(sinfo, filter = list(position = "top", clear = FALSE, plain = FALSE),
+      rownames = FALSE, style = "bootstrap", escape = FALSE,
+      options = list(
+        search = list(regex = TRUE),
+        columnDefs = list(list(className = 'dt-center', targets = "_all")),
+        autoWidth = TRUE,
+        processing = FALSE,
+        pageLength = 10,
+        lengthMenu = list(c(10, 25, 50, -1), c('10','25','50','All'))
+      )
+    )
+  })
+}
+
+########################################
+## function below not currently used
+########################################
 cold_call_shiny <- function(sinfo, nr = 5) {
+
   shinyApp(
     ui = fluidPage(
       tags$head(
@@ -66,24 +114,24 @@ cold_call_shiny <- function(sinfo, nr = 5) {
           }"))
       ),
       fluidRow(
-        actionButton("cold_call_button", "Cold call shiny")
+        actionButton("cold_call_button_shiny", "Cold call shiny")
       ),
       fluidRow(
-        tableOutput("cold_call_table")
-        # htmlOutput("cold_call_xtable")
-        # , DT::dataTableOutput("cold_call_DT")
+        tableOutput("cold_call_table"),
+        htmlOutput("cold_call_xtable"),
+        DT::dataTableOutput("cold_call_DT")
       )
     ),
 
     server = function(input, output, session) {
 
       output$cold_call_table <- renderTable({
-        if(input$cold_call_button == 0) return()
+        if(input$cold_call_button_shiny == 0) return()
         cold_call(sinfo, nr = nr)
       }, sanitize.text.function = identity)
 
       output$cold_call_xtable <- renderText({
-        if(input$cold_call_button == 0) return()
+        if(input$cold_call_button_shiny == 0) return()
         cold_call(sinfo, nr = nr) %>%
         xtable::xtable(.) %>%
         print(type = "html",  print.results = FALSE, include.rownames = FALSE,
@@ -93,7 +141,7 @@ cold_call_shiny <- function(sinfo, nr = 5) {
       })
 
       output$cold_call_DT <- DT::renderDataTable({
-        if(input$cold_call_button == 0) return()
+        if(input$cold_call_button_shiny == 0) return()
         cold_call(sinfo, nr = nr) %>%
           DT::datatable(rownames = FALSE, style = "bootstrap", escape = FALSE,
             options = list(
